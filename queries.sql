ce9982f91b298ec514aa60f2c2a6249b5dd12176
Eddie Jimenez <52768061+l8nightswithJS@users.noreply.github.com>
2020-04-16 19:14:52 -0700

    MVP

--- queries.sql
+++ queries.sql
@@ -2,9 +2,9 @@
 SELECT * FROM [Order]
 -- Display the ProductName and CategoryName for all products in the database. Shows 77 records.
 SELECT ProductName, CategoryName
-    FROM Product
-    JOIN Category
-    ON Product.CategoryId = Category.Id
+    FROM Product P
+    JOIN Category C
+    ON P.CategoryId = C.Id
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
 SELECT O.Id, S.CompanyName
     FROM [Order] O
---
+++ schemes/scheme-model.js
@@ -0,0 +1,48 @@
+module.exports = {
+    find,
+    findById,
+    findSteps,
+    add,
+    addStep,
+    update,
+    remove,
+    addStep
+};
+
+function find() {
+    return db('schemes')
+}
+
+function findById(id) {
+    return db('schemes')
+        .where({ id })
+        .first()
+}
+
+function findSteps(id) {
+    return db('steps')
+        .where({scheme_id: id})
+        .insert(stepData);
+}
+
+function add(schemeData) {
+    return db('schemes').insert(schemeData);
+}
+  
+  function addStep(stepData, id) {
+    return db('steps')
+        .where({scheme_id: id})
+        .insert(stepData);
+}
+
+function update(changes, id) {
+    return db('schemes')
+        .where({ id })
+        .update(changes);
+}
+
+function remove(id) {
+    return db('schemes')
+        .where({ id })
+        .del()
+}
